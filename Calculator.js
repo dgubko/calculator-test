3537f9a9f89cf2b5adbdeb45986d71992862bcf6
Dmitrii Gubko <dalnoboy29region@gmail.com>
2022-08-22 18:19:47 -0600

    Add operators

--- Calculator.js
+++ Calculator.js
@@ -2,5 +2,32 @@ class Calculator {
   constructor() {
     this.numbers = [];
     this.operations = [];
+    this.prevResult = 0;
+  }
+  plus(num) {
+    return this.prevResult + num;
+  }
+  minus(num) {
+    return this.prevResult - num;
+  }
+  multiplication(num) {
+    return this.prevResult * num;
+  }
+  devision(num) {
+    return this.prevResult / num;
+  }
+  addOperation(oper) {
+    this.operations.push(oper);
+  }
+  addNumber(num) {
+    this.numbers.push(num);
+  }
+  replaceOper(oper) {
+    this.operations[this.operations.length - 1] = oper;
+  }
+  clean() {
+    this.operations = [];
+    this.numbers = [];
+    this.prevResult = 0;
   }
 }
--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+var calc = new Calculator();
+console.log(calc);
+var operButtons = document.getElementsByClassName("operButton");
+for (var i = 0; i < operButtons.length; i++) {
+  operButtons[i].addEventListener("click", function (event) {
+    var lastChar = screen.innerText[screen.innerText.length - 1];
+    if (lastChar !== "\xa0") {
+      calc.addOperation(event.target.id);
+      document.getElementById(
+        "screen"
+      ).innerText = `${screen.innerText} ${event.target.innerText}\xa0`;
+    } else {
+      var newText = screen.innerText;
+      calc.replaceOper(event.target.id);
+
+      document.getElementById("screen").innerText =
+        newText.slice(0, -2) + `${event.target.innerText}\xa0`;
+      console.log(calc.operations);
+    }
+  });
+}
 var screen = document.getElementById("screen");
 function passNumber(number) {
   var isEmpty = screen.innerText === "0";
@@ -17,12 +38,10 @@ for (var i = 0; i < numbers.length; i++) {
 
 document.getElementById("AC").addEventListener("click", function () {
   document.getElementById("screen").innerText = 0;
+  calc.clean();
 });
 
 // ### PLUS ###
 function sum(a, b) {
   return a + b;
 }
-document.getElementById("plus").addEventListener("click", function () {
-  document.getElementById("screen").innerText = `${screen.innerText} +\xa0`;
-});
